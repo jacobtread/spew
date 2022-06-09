0036e71627903a57819c571cd32a27672cd043d0
Jacobtread <jacobtread@gmail.com>
2022-06-10 09:22:54 +1200

    More parsing logic

--- src/main.rs
+++ src/main.rs
@@ -24,25 +24,67 @@ enum ExpressionType {
 #[derive(Debug)]
 enum Token {
     Whitespace,
+    Comment(String),
     Ident(String),
     Block(Vec<Token>),
     Expression(Box<Token>, Box<Token>, ExpressionType),
 }
 
+#[derive(Debug)]
+enum ParserError {
+    UnexpectedToken(char, String),
+    Expected(String),
+}
+
+type ParseResult<T> = Result<T, ParserError>;
+
 impl Parser {
-    fn parse(text: &str) -> Vec<Token> {
+    fn parse(text: &str) -> ParseResult<Vec<Token>> {
         let mut tokens = Vec::new();
         let mut chars = text.chars();
-        while let Some(mut next_char) = chars.next() {
-            if next_char.is_whitespace() { // Encountered whitespace
+        let column = 0;
+        let line = 0;
+        'parse_loop: while let Some(mut next_char) = chars.next() {
+            // Whitespace consumption
+            if next_char.is_whitespace() {
+                tokens.push(Token::Whitespace); // Add whitespace token
                 while let Some(char) = chars.next() { // Consume all whitespace
                     if !char.is_whitespace() {
                         next_char = char;
-                        break;
+                        continue 'parse_loop;
                     }
                 }
-                tokens.push(Token::Whitespace);
             }
+
+            // Start Comment consumption
+
+            // Line Comment
+            if next_char == '/' {
+                if let Some(char) = chars.next() {
+                    if char == '/' { // Full line comment
+                        let mut comment_text = String::new();
+                        while let Some(char) = chars.next() {
+                            if char != '\n' {
+                                comment_text.push(char)
+                            } else {
+                                next_char = char;
+                                break
+                            }
+                        }
+                        tokens.push(Token::Comment(comment_text))
+                    } else if next_char == '*' { // Multi-line comment
+                        todo!("Implement multiline comment")
+                    } else {
+                        return Err(ParserError::UnexpectedToken(next_char, String::from("Expected '/' for line comment or '*' for multiline comment")));
+                    }
+                } else {
+                    return Err(ParserError::Expected(String::from("/ for comment at")));
+                }
+            }
+
+            // End Comment consumption
+
+
             if !next_char.is_whitespace() {
                 if next_char.is_alphabetic() { // Try parsing ident
                     let mut ident = String::from(next_char);
@@ -58,13 +100,19 @@ impl Parser {
                 }
             }
         }
-        return tokens;
+        return Ok(tokens);
     }
 }
 
 const SRC: &str = "hello world123       test";
 
+const SOURCE: &str = concat!(
+    "// Test this is a comment",
+    "might work like this",
+);
+
 fn main() {
-    let tokens = Parser::parse(SRC);
+    let tokens = Parser::parse(SOURCE)
+        .expect("Failed to parse");
     println!("{:?}", tokens);
 }
